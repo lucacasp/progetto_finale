f2bf68995b7a92cd1cb3db575ac9177c42a10319
Luca Casparriello <155079990+lucacasp@users.noreply.github.com>
2024-09-13 18:05:11 +0200

    rimosso dipendenza e importazione slick carousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,6 @@ import ShopCategory from "./Pages/ShopCategory";
 import eyewear_banner from "./Components/Assets/eyewear_banner.png";
 import glassify_banner_sunglasses_cropped2 from "./Components/Assets/glassify_banner_sunglasses_cropped2.png";
 import LoginSignup from "./Pages/LoginSignup";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 export const backend_url = 'hhttps://glassyecommerce.herokuapp.com/';
 export const currency = '$';
