152a647b9218ca46e34b956cb422b603665a8a21
Luca Casparriello <155079990+lucacasp@users.noreply.github.com>
2024-09-13 02:55:59 +0200

    si

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,14 @@ const path = require("path");
 const cors = require("cors");
 const port = process.env.PORT || 4000;
 
-
-app.use(express.json());
-app.use(cors());
-
-// Database Connection With MongoDB
-mongoose.connect("mongodb+srv://lucacasp:admin123@cluster0.20gkypm.mongodb.net/");
+require("dotenv").config();
+// Connessione al database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connessione al database MongoDB avvenuta con successo"))
+.catch((err) => console.error("Errore di connessione al database:", err));
 
 
 //Image Storage Engine 
